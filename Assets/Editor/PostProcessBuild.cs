3baa9320bc0700c9b3e2aeb7367542f90c02be0a
Tomas Dirvanauskas <tomas@unity3d.com>
2019-07-15 13:42:48 +0300

    Add PostProcessBuild.cs

---
+++ Assets/Editor/PostProcessBuild.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEditor.Build;
+using UnityEngine;
+using System.IO;
+using UnityEditor.Build.Reporting;
+
+
+class MyCustomBuildProcessor : IPreprocessBuildWithReport
+{
+    public int callbackOrder { get { return 0; } }
+    public void OnPreprocessBuild(BuildReport report)
+    {
+        Directory.CreateDirectory(Path.Combine(Application.dataPath, "StreamingAssets"));
+        File.WriteAllText(Configuration.ConfigurationPath, JsonUtility.ToJson(Configuration.Instance));
+    }
+}
